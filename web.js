8452ddf44d71f93b01d758adef33cdf7c0277a5b
= <=>
2013-07-09 19:10:05 +0400

    upd web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var buffer = fs.readFileSync("./index.html");
+    var string = buffer.toString();
+
+  response.send(string);
+
 });
 
 var port = process.env.PORT || 5000;
